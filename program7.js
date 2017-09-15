abe7dc3844d671c063e92cca157f7d4bd239d6fc
Adham EL Banhawy <the.benhawy@gmail.com>
2017-09-15 11:44:20 -0500

    Program uses get request to fetch response object

---
+++ program7.js
@@ -0,0 +1,12 @@
+/* A program that performs an HTTP GET request to a URL provided to you  
+  as the first command-line argument. Write the String contents of each  
+  "data" event from the response to a new line on the console (stdout).*/
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function callback (response){
+	response.setEncoding("utf8")
+	response.on("data", console.log);
+	response.on("error", console.error);
+    }
+    ).on('error', console.error);
